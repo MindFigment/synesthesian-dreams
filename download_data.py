3e1d6038d48542280b2bd0c61d4989b087a65cb9
MindFigment <maksymilian.szmelczynski@gmail.com>
2019-11-14 20:44:32 +0100

    Download data script

--- download_data.py
+++ download_data.py
@@ -1,19 +1,32 @@
 import requests
 import zipfile
 import os
+from tqdm import tqdm
 
 wikiart_zip =  "http://web.fsktm.um.edu.my/~cschan/source/ICIP2017/wikiart.zip"
-# file_path = "/home/stanislaw/Downloads/Humpback-Whale-Identification-master.zip"
 dir_path = "./data"
 
-r = requests.get(wikiart_zip)
+r = requests.get(wikiart_zip, stream=True)
 
-with zipfile.ZipFile(r.content) as zf:
+zip_size = int(r.headers['Content-Length'].strip())
+print("Zip size: {} GB".format(zip_size / 1073741824)) # Bytes / (Number of Bytes in Gigabyte)
+block_size = 1024
+with open("tmp.zip", "wb") as f:
+    with tqdm(total=zip_size, unit="iB", unit_scale=True) as t:
+        for data in r.iter_content(block_size):
+            t.update(len(data)) 
+            f.write(data) 
+with zipfile.ZipFile(f) as zf:
     try:
         os.mkdir(dir_path)
         print("Directory {} created".format(dir_path.split("/")[1]))
     except FileExistsError:
         print("Directory {} already exists".format(dir_path))
-    zf.extractall(path=dir_path)
+    for member in tqdm(zf.infolist(), desc="Extracting "):
+	    try:
+	        zf.extract(member, dir_path)
+	    except zipfile.error as e:
+	        print("Something went wrong {}".format(e))
+    # zf.extractall(path=dir_path)
     print("File {} extrated into {}".format(wikiart_zip.split("/")[-1], dir_path.split("/")[1]))
 
