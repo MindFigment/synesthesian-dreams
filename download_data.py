4be8d4f2453c58b0a7b2f806646e0bc46b753dc6
mindfigment <maksymilian.szmelczynski@gmail.com>
2019-11-14 18:24:34 +0100

    .

--- download_data.py
+++ download_data.py
@@ -2,15 +2,18 @@ import requests
 import zipfile
 import os
 
-file_path = "/home/stanislaw/Downloads/Humpback-Whale-Identification-master.zip"
+wikiart_zip =  "http://web.fsktm.um.edu.my/~cschan/source/ICIP2017/wikiart.zip"
+# file_path = "/home/stanislaw/Downloads/Humpback-Whale-Identification-master.zip"
 dir_path = "./data"
 
-with zipfile.ZipFile(file_path) as zf:
+r = requests.get(wikiart_zip)
+
+with zipfile.ZipFile(r.content) as zf:
     try:
         os.mkdir(dir_path)
         print("Directory {} created".format(dir_path.split("/")[1]))
     except FileExistsError:
         print("Directory {} already exists".format(dir_path))
     zf.extractall(path=dir_path)
-    print("File {} extrated into {}".format(file_path.split("/")[-1], dir_path.split("/")[1]))
+    print("File {} extrated into {}".format(wikiart_zip.split("/")[-1], dir_path.split("/")[1]))
 
