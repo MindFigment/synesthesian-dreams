ee5f1628e1539e6e9a93019beaac70e4e2420509
mindfigment <maksymilian.szmelczynski@gmail.com>
2019-11-26 21:05:30 +0100

    .

--- pytorch_version/train.py
+++ pytorch_version/train.py
@@ -7,7 +7,9 @@ from utils import weights_init
 import torch.optim as optim
 import torchvision.utils as vutils
 import torch.nn as nn
-
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # Numbers of workers for dataloader
 workers = 2
@@ -182,3 +184,38 @@ img_list.        b_size = real_cpu.size(0)
 
 
     
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+# plt.show()
+plt.savefig('../images/losses_plot.png')
+
+fig = plt.figure(figsize=(8,8))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+anim = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+
+# plt.show()
+anim.save('../images/animation.gif', writer='imagemagick', dpi=30)
+
+# Grab a batch of real images from the dataloader
+real_batch = next(iter(dataloader))
+
+# Plot the real images
+plt.figure(figsize=(15,15))
+plt.subplot(1,2,1)
+plt.axis("off")
+plt.title("Real Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+
+# Plot the fake images from the last epoch
+plt.subplot(1,2,2)
+plt.axis("off")
+plt.title("Fake Images")
+plt.imshow(np.transpose(img_list[-1],(1,2,0)))
+# plt.show()
+plt.savefig('../images/fake_vs_real.png')
