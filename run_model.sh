781d380667833cb0127d0267219322eaa6cbafa0
MindFigment <maksymilian.szmelczynski@gmail.com>
2019-12-02 22:02:34 +0100

    .

--- run_model.sh
+++ run_model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python train.py --epochs=200 --batch_size=256 --nz=100 --use_gpu \
+python train.py --epochs=300 --batch_size=256 --nz=100 --use_gpu \
                 --model_dir=first_model --model_name=first_model \
                 --save_model_every=50 --check_gen_every=10
